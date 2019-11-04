06adcce48c90498329a0da08e1475a2b42cc33cc
rithaven <veneruth4@gmail.com>
2019-11-04 10:03:19 +0200

    Add urls paths

--- neighborapp/urls.py
+++ neighborapp/urls.py
@@ -3,6 +3,9 @@ from .import views
 
 
 urlpatterns=[
-  url(r'^$', views.home, name='home')
-  url(r'^neighborhood/(/d+)',views.neighborhood,name)
+  url(r'^$', views.home, name='home'),
+  url(r'^neighborhood/(/d+)',views.neighborhood,name=neighborhood),
+  url(r'^profile/(\d+)',views.profile,name='profile'),
+  url(r'^add_business/',views.add_business,name='add_business'),
+  url(r'^change_neighborhood/(\d+)',)
 ]
