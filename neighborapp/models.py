4f01c2ce11c9d16f81ab78291475594ae65a8522
rithaven <veneruth4@gmail.com>
2019-11-04 09:33:45 +0200

    Add forms file to the project

--- neighborapp/models.py
+++ neighborapp/models.py
@@ -1,3 +1,84 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Neighborhood(models.Model):
+      neighborhood_name = models.CharField(max_length=30)
+      def Create_neighborhood(self):
+        self.save()
+
+      def delete_neighborhood(self):
+         self.delete()
+
+      @classmethod
+      def find_neighborhood(cls,neighborhood_id):
+          neighborhood =cls.objects.get(id=neighborhood_id)
+          return neighborhood
+
+      def update_neighborhood(self,name):
+          self.name = name
+          self.save()
+
+      def __str__(self):
+          return f'{self.neighborhood_name}'
+
+class UserProfile(models.Model):
+      first_name =models.CharField(max_length=20, blank=True)
+      last_name = models.CharField(max_length=20, blank=True)
+      user= models.ForeignKey(User,on_delete= models.CASCADE)
+      location= models.CharField(max_length=30, blank=True)
+
+      def assign_neighborhood(self,neighborhood):
+          self.neighborhood = neighborhood
+          self.save()
+      def save_profile(self):
+          self.save()
+      def delete_profile(self):
+          self.delete()
+      def __str__(self):
+        return f'{self.user.username}'
+
+class Business(models.Model):
+    name = models.CharField(max_length=30)
+    BusinessOwner = models.ForeignKey(User, on_delete=models.CASCADE)
+    B_location = models.CharField(max_length=30, blank=True)
+    b_neighborhood= models.ForeignKey('Neighborhood',on_delete=models.CASCADE)
+    email = models.EmailField()
+
+    def create_business(self):
+        self.save()
+    def delete_business(self):
+        self.delete()
+
+    @classmethod
+    def find_business(cls,business_id):
+        business =cls.objects.get(id=business_id)
+        return business
+    def update_business(self,name):
+        self.name =name
+        self.save()
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Contacts(models.Model):
+      name = models.CharField(max_length=30)
+      contacts= models.CharField(max_length=30)
+      email = models.EmailField()
+      neighborhood_contact = models.ForeignKey('Neighborhood',on_delete=models.CASCADE)
+
+      def __str__(self):
+          return f'{self.name},{self.email}'
+
+class Post(models.Model):
+      title =models.CharField(max_length=40)
+      description = HTMLField()
+      posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+      neighbors= models.ForeignKey('Neighborhood',on_delete=models.CASCADE)
+      postDate= models.DateTimeField(auto_now_add=True)
+
+      def __str__(self):
+          return f'{self.title},{self.post_hood.neighborhood_name}'
+
--- neighborapp/views.py
+++ neighborapp/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
+from .models import UserProfile, Neighborhood, Business,EmergencyContacts,Post
+from .forms import NeighborhoodForm,UpdateProfileForm,AddBusinessForm,PostForm
+from django.contrib.auth.models import User
+import datetime
 
 # Create your views here.
+def home(request):
+    if no request.user.is_authenticated:
+          return redirect('signout')
+    else:
+      if request.user.id==1:
+         if request.method == 'POST':
+            form= NeighborhoodForm(request.POST)
+            if form.is_valid():
+                neighborhood= Neighborhood(neighborhood_name=request.POST['neighborhood_name'],neighborhood_location=request.POST['neighborhood_location'])
+                neighborhood.save()
+            return redirect('index')
+         else:
+              form= NeighborhoodForm()
+         neighborhoods = Neighborhood.objects.all()
+         return render(request,'Home.html',{'neighborhoods,'form':form})
+      elif request.user !=1:
+          user = UserProfile.objects.filter(user=request.user).first()
+          if user is None:
+              user = UserProfile(user=request.user) 
+              user.save()
+          if user.neighborhood is None:
+               title='Neighborhood'
+               neighbors= Neighborhood.objects.all()
+               return render(request,'Home.html',{'neighbors':neighbors})
+          else: 
+              user= UserProfile.objects.filter(user= request.user).first()
+              return redirect(reverse('neighborhood',args=[user.neighborhood.id]))
+def neighborhood(request, neighborhood_id):
+    if request.user.id == 1:
+         neighborhood = Neighborhood.objects.get(id = neighborhood_id)
+         partners = UserProfile.objects.filter(neighborhood =neighborhood).all()
+         emergencies = Contacts.objects.filter(neighborhood_contact = neighborhood).all()
+         return render(request,'neighbors.html',{'neighborhood':neighborhood,'members':members,'emergencies':emergencies})
+    else:
+        neighborhood = Neighborhood.objects.get(id = neighborhood_id)
+        user= UserProfile.objects.filter(user = request.user).first()
+        businesses = Business.objects.filter(business_neighborhood=neighborhood).all()
+        emergencies = Contacts.objects.filter(neighborhood_contact = neighborhood).all()
+        user.neighborhood = neighborhood
+        user.save()
+
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = Post(title=request.POST['title'],post_description=request.POST['post_description'],posted_by=request.user,post_hood=neighborhood,posted_on=datetime.datetime.now())
+                post.save()
+                return redirect(reverse('neighborhood',args=[neighborhood.id]))
+            else:
+                 form=PostForm()
+            posts= Post.objects.filter(post_hood= neighborhood).all()
+            return render(request,'neighbors.html',{'posts':posts,'form':form,'user':user,'businesses':businesses,'neighborhood':neighborhood,'emergencies':emergencies})
+
+def profile(request,user_id):
+    user = User.objects.get(id= user_id)
+    profile = UserProfile.objects.filter(user=user).filter()
+    if request.method =='Post':
+        form =UpdateProfileForm(request.POST,instance=profile)
+        if form.is_valid():
+            profile.first_name =request.POST['first_name']
+            profile.last_name =request.POST['last_name']
+            profile.location = request.POST['location']
+            profile.save()
+        return redirect(reverse'profile',args=[user.id])
+    else:
+         form: UpdateProfileForm(instance=profile)
+         
