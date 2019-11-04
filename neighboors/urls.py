9b99593fdc4f144cdbb2a8de995dbd64676346f5
rithaven <veneruth4@gmail.com>
2019-11-04 10:28:09 +0200

    Add  app to the settings

--- neighboors/urls.py
+++ neighboors/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('neighborapp.uls')),
-    url(r'accounts/',include('django'))
+    url(r'accounts/',include('django.contrib.auth.urls')),
 ]
